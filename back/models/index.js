6f6800e8d88f5f378b18de493a5451fb6f14e223
sohyeonBak <sohyeonbakoly@gmail.com>
2021-02-25 15:17:02 +0900

    hashtag change

--- back/models/index.js
+++ back/models/index.js
@@ -7,7 +7,7 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.Comment = require('./comment')(sequelize,Sequelize);
-db.Hashtag = require('./Hashtag')(sequelize,Sequelize);
+db.Hashtag = require('./hashtag')(sequelize,Sequelize);
 db.Image = require('./image')(sequelize,Sequelize);
 db.Post = require('./post')(sequelize,Sequelize);
 db.User = require('./user')(sequelize,Sequelize);
