b2a85368fb0496011c23af11b4991aad28faa05d
sohyeonBak <sohyeonbakoly@gmail.com>
2021-02-25 17:01:43 +0900

    app change

--- back/app.js
+++ back/app.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const path =require('path')
+const helmet =require('helmet')
+const hpp =require('hpp')
 
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
@@ -25,7 +27,13 @@ db.sequelize.sync()
 
 passportConfig();
 
-app.use(morgan('dev'))
+if(process.env.NODE_ENV === 'production'){
+    app.use(morgan('combined'))
+    app.use(hpp());
+    app.use(helmet())
+}else{
+    app.use(morgan('dev'))
+}
 app.use(cors({
     origin: true,
     credentials: true,
@@ -56,6 +64,6 @@ app.use('/post', postRouter);
 app.use('/user', userRouter);
 app.use('/hashtag', hashtagRouter);
 
-app.listen(3065, ()=>{
+app.listen(80, ()=>{
     console.log('서버 실행 중')
 })
