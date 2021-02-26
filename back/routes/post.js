5cb413f7b290230ff0a4a199022e66bcfa45ef7e
sohyeonBak <sohyeonbakoly@gmail.com>
2021-02-26 15:12:15 +0900

    code fix

--- back/routes/post.js
+++ back/routes/post.js
@@ -87,7 +87,7 @@ router.post('/', isLoggedIn, upload.none(), async (req, res, next)=>{
 
 router.post('/images', isLoggedIn, upload.array('image'), (req,res,next)=>{
   console.log(req.files);
-  res.json(req.files.map((v)=> v.location(/\/original\//, '/thumb/')))
+  res.json(req.files.map((v)=> v.location.replace(/\/original\//, '/thumb/')))
 })
 
 
