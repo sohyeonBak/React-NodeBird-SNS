2ce3b5c79f0cb81305b6c2c60958c1fc5f06f793
sohyeonBak <sohyeonbakoly@gmail.com>
2021-02-26 14:41:19 +0900

    thumb change

--- front/components/imageszoom.js/index.js
+++ front/components/imageszoom.js/index.js
@@ -27,7 +27,7 @@ const ImagesZoom = ({ images, onClose }) => {
           >
             {images.map((value) => (
               <ImgWrapper key={value.src}>
-                <img src={`${value.src}`} alt={value.src} />
+                <img src={`${value.src.replace(/\/thumb\//, '/original/')}`} alt={value.src} />
               </ImgWrapper>
             ))}
           </Slick>
